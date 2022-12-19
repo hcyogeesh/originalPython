a708989d0b14c8ffa1a3b5a30b278251fc176109
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-19 19:44:03 +0530

    newCommits @ 2022-12-19-19:44:02

--- Python/veryEasy/[12] Power Calculator [VE].py
+++ Python/veryEasy/[12] Power Calculator [VE].py
@@ -45,3 +45,9 @@ _________
 """ 
 # Your code should go here:
 
+def circuitPower(voltage, current):
+    return (voltage * current)
+
+print(circuitPower(230,10))
+print(circuitPower(110,3))
+print(circuitPower(480,20))
