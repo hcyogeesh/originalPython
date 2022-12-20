2c71c46221a3cd95813d2fa6b9b268015110b142
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-21 01:20:01 +0530

    newCommits @ 2022-12-21-01:20:01

--- Python/veryEasy/[61] Even or Odd_ [VE].py
+++ Python/veryEasy/[61] Even or Odd_ [VE].py
@@ -49,3 +49,20 @@ _________
 """ 
 # Your code should go here:
 
+def sumListInt(list1):
+    
+# Given a list of integers, determine whether the sum of its elements is even or odd.
+# The return value should be a string ("odd" or "even").
+# If the input list is empty, consider it as a list with a zero ([0]).
+#
+#
+# [Examples]
+#
+# ___
+# even_or_odd([0]) ➞ "even"
+#
+# even_or_odd([1]) ➞ "odd"
+#
+# even_or_odd([]) ➞ "even"
+#
+# even_or_odd([0, 1, 5]) ➞ "even"
