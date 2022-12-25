3351d87e1b3f8b3356bf55ad20490e31745755ec
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-26 03:00:02 +0530

    newCommits @ 2022-12-26-03:00:02

--- Python/veryEasy/[107] Many Operators! [VE].py
+++ Python/veryEasy/[107] Many Operators! [VE].py
@@ -63,22 +63,37 @@ _________
 def switchMethod(num1, num2, operator):
     match operator:
         case "+" | "-" | "%":
-            # return "Hello"
-            print(("{0}{1}{2}".format(num1, operator, num2)))
+            return eval("{0}{1}{2}".format(num1, operator, num2))
         case _:
-            print("Please enter the operator from the given.")
+            return "Please enter the operator from the given."
 
-print(switchMethod(1, 2, "*"))
+print(switchMethod(1, 2, "-"))
+print(switchMethod(2, 2, "+"))
+print(switchMethod(3, 3, "%"))
+print(switchMethod(10, 5, "-"))
+print(switchMethod(103, 10, "%"))
 
-def matchCaseTest():
-    responseCode = 400
-    match responseCode:
-        case 400:
-            return "Hello, 400 is returned."
-        case 401 | 402:
-            return "Hello, 403"
-# 1 + 2 = 3
-# operate(7, 10, "-") ➞ -3
-# 7 - 10 = -3
-# operate(20, 10, "%") ➞ 0
-# 20 % 10 = 0
+# The program is complete.
+
+
+
+# Just test function.
+# def matchCaseTest(respCode):
+#     match respCode:
+#         case 400:
+#             return "Hello, 400 is returned."
+#         case 401 | 402:
+#             return "Hello, 401 and 402 is returned."
+#         case 403:
+#             return "Hello 403 is returned."
+#         case 404:
+#             return "Hello 404 is returned."
+#         case 405:
+#             return "Hello, 405 is returned."
+#         case _:
+#             return "Yikes, fuck you bitch"
+#
+# print(matchCaseTest(100))
+# print(matchCaseTest(404))
+# print(matchCaseTest(401))
+# print(matchCaseTest(402))
