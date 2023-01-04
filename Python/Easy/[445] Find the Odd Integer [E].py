ae62c4b5bd96739aa5e619abdc0d0d2cc9df2bb6
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-04 13:45:05 +0530

    newCommits @ 2023-01-04-13:45:04

--- Python/Easy/[445] Find the Odd Integer [E].py
+++ Python/Easy/[445] Find the Odd Integer [E].py
@@ -50,3 +50,20 @@ _________
 """ 
 # Your code should go here:
 
+def oddCountIndex(list1):
+    
+
+# find_odd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]) ➞ -1
+#
+#
+# find_odd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]) ➞ 5
+#
+# find_odd([10]) ➞ 10
+# _____
+#
+#
+#
+# [Notes]
+#
+# There will always only be one integer that appears an odd number of times.
+#
